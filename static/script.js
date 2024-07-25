0d90c40732f27fed73fba8e32e2712f92ce22c93
sukanta <sukanta.roy@arndit.com>
2024-07-25 18:06:39 +0530

    Nav Bar, Update Task section, All task, delete options updated

--- static/script.js
+++ static/script.js
@@ -11,27 +11,12 @@ document.querySelector('.todo').addEventListener('click',(e)=>{
     window.location.href='/';
 })
 function taskDetails(id){
-    window.location.href=`taskDetails/${id}`
+    window.location.href=`/taskDetails/${id}`
 }
 function delete_task(id) {
+    console.log("hello");
     if(confirm("are you sure you want to delete?")){
         console.log(`delete_task/${id}`);
-        window.location.href=`delete_task/${id}`;
-    }
-}
-function del_all(){
-    console.log("clicked");
-    let str="Type 'Delete All' to confirm"
-    while(true){
-        let res=prompt(str)
-        if (res == 'Delete All'){
-            console.log('correct');
-            window.location.href='/deleteAll';
-            break;
-        }
-        else if(res!='' && !res)
-            break;
-        else
-            str="Type Correctly: 'Delete All' to confirm"
+        window.location.href=`/delete_task/${id}`;
     }
 }
--- tasks/views.py
+++ tasks/views.py
@@ -1,29 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Tasks
-# data=[
-#         {
-#             'id':1,
-#             'title': 'Finishing Assignment',
-#             'description': 'make a todo App with Django, showcasing CRUD operations',
-#             'dateTime': '17-07-2024 18:00',
-#             'status': True
-#         },
-#         {
-#             'id':2,
-#             'title': 'goto market',
-#             'description': 'buy fish, grocery etc',
-#             'dateTime': '19-07-2024 9:00',
-#             'status': False
-#         },
-#         {
-#             'id':3,
-#             'title': 'submit project',
-#             'description': 'deadline is EOD today',
-#             'dateTime': '16-07-2024 18:00',
-#             'status': False
-#         },
-#     ]
 
 def index(request):
     if request.method == 'POST':
@@ -44,6 +21,14 @@ def index(request):
         # return HttpResponse("hello this is index page")
 
 def taskList(request):
+    # print(request.path)
+    if request.method == 'POST':
+        data=request.POST
+        print(data['status']+" "+data['id'])
+        Tasks.objects.filter(id=data['id']).update(
+            status=data['status']
+        )
+        return redirect(request.path)
     data= Tasks.objects.all()
     context={
         'page': 'taskList',
@@ -52,6 +37,13 @@ def taskList(request):
     return render(request,'taskList.html',context)
 
 def taskDetails(request,id):
+    if request.method == 'POST':
+        data=request.POST
+        print(data['status']+" "+data['id'])
+        Tasks.objects.filter(id=data['id']).update(
+            status=data['status']
+        )
+        return redirect(request.path)
     data=Tasks.objects.get(id=id)
     return render(request,'taskDetails.html',context={'data':data,})
 
@@ -61,8 +53,10 @@ def update_task(request,id):
         Tasks.objects.filter(id=id).update(
             title=data['title'],
             description=data['description'],
+            status=data['status'],
         )
-        return render(request,'updateTask.html',context={'message':'success', 'data': data,})
+        data=Tasks.objects.filter(id=id)
+        return render(request,'taskDetails.html',context={'message':'success', 'data': data[0],})
     else:
         data=Tasks.objects.get(id=id)
         return render(request,'updateTask.html',context={'data':data})
@@ -85,12 +79,6 @@ def addTask(request):
     # return HttpResponse("hello this is add Task page")
 
 def del_task(request,id):
+    print(id)
     Tasks.objects.get(id = id).delete()
-    
     return redirect('/')
-
-def del_all(request):
-    Tasks.objects.all().delete()
-    # print(request.META.HTTP_REFERER)
-    # print()
-    return redirect('/tasklist')
